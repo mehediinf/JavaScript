1d9ec8b3c400fc9743c73462bf493bb57bcf867b
Mehedi Hasan <mehediinform@gmail.com>
2024-09-06 01:08:42 +0600

    1st Commit

---
+++ program.js
@@ -0,0 +1,62 @@
+
+var fName = prompt("First Name: ");
+var lName = prompt("Last Name: ");
+
+var fullName = fName +" "+ lName;
+
+document.write(fullName+"</br>");
+
+document.write(fullName.length+"</br>");
+
+document.write(fullName.toUpperCase()+"</br>");
+document.write(fullName.toLowerCase()+"</br>");
+
+document.write(fullName.charAt(2)+"</br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Type Convertion .........
+// var str = "45";
+// var num = 2221091045;
+// var flt = 45.4512;
+
+// str = parseInt(str);
+// document.write(typeof(str)+"</br>");
+
+// num = toString(num);
+// document.write(typeof(num)+"</br>");
+
+// flt = toString(flt);
+// document.write(typeof(flt)+"</br> </br>");
+
+// var numFixed = 45.452;
+
+// document.write(numFixed.toFixed(2)+"</br>");
+// document.write(numFixed.toPrecision(6)+"</br>");
+
+// var number = "452.544";
+// document.write(number+"</br>");
+// document.write(typeof(Number(number)));
